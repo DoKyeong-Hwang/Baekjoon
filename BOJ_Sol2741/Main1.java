f32b4ef8cbad4d2c5c765ef26622ca64dee3da1d
DoKyeong-Hwang <dokyung9402@gmail.com>
2021-08-02 17:58:43 +0900

    20210802

---
+++ BOJ_Sol2741/Main1.java
@@ -0,0 +1,16 @@
+package BOJ_Sol2741;
+import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+
+public class Main1 {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        in.close();
+
+        for (int i = 1; i <= n; i++){
+            System.out.println(i);
+        }
+    }
+}
