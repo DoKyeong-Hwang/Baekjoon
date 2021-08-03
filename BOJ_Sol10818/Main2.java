19026741e0c50e8180908648b89e922b5cddd2dd
DoKyeong-Hwang <dokyung9402@gmail.com>
2021-08-03 19:01:34 +0900

    baekjoon 10818,1152

---
+++ BOJ_Sol10818/Main2.java
@@ -0,0 +1,25 @@
+package BOJ_Sol10818;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main2 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int index = 0;
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int[] arr = new int[n];
+        while (st.hasMoreTokens()){
+            arr[index] = Integer.parseInt(st.nextToken());
+            index++;
+        }
+
+        Arrays.sort(arr);
+        System.out.println(arr[0] + " " + arr[n-1]);
+    }
+}
---
+++ BOJ_Sol1152/Main1.java
@@ -0,0 +1,16 @@
+package BOJ_Sol1152;
+
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Main1 {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        String s = in.nextLine();
+        in.close();
+
+        StringTokenizer st = new StringTokenizer(s, " ");
+
+        System.out.println(st.countTokens());
+    }
+}
